b7f00e485ae8ee4b33e435187bce44ea90f8f878
Jake Surgeon <jakobsurge@gmail.com>
2024-10-29 23:26:22 -0400

    Got a start to the flask app. Nothing crazy just messing with Flask as an API basics

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Get request example
+@app.route("/get-piratified-text/")
+def get_encrypted_text():
+    plain_text = request.args.get("plaintext")
+    key = request.args.get("key")
+
+    return __pirAtES__(plain_text, key), 200
+
+# Function that actually does the encryption/pirate substitution
+def __pirAtES__(plain_text, key):
+    
+    
+    return "piratified_text"
+
+if __name__ == "__main__":
+    app.run(debug=True)
