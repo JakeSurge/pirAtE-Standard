1c42c2297d8bbffa863fa9cbcdbe0585b27247fd
Jake Surgeon <jakobsurge@gmail.com>
2024-10-30 12:04:52 -0400

    I think I have encryption working and maybe decryption should hypothetically work but cannot get data types to play well with whatever C extensions the cryptodome package uses

--- main.py
+++ main.py
@@ -1,29 +1,55 @@
 from flask import Flask, request, jsonify
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import pad, unpad
 
 import wordset
 
 app = Flask(__name__)
 
-# Get request example
+# Get request for encryption
 @app.route("/get-piratified-text/")
 def get_encrypted_text():
     plain_text = request.args.get("plaintext")
     key = request.args.get("key")
 
-    return jsonify(__pirAtES__(plain_text, key)), 200
+    return __pirAtES__(plain_text, key)
 
-# Function that actually does the encryption/pirate substitution
+# Function that does the encryption/pirate substitution
 def __pirAtES__(plain_text, key):
+    # Encode args so they play well with C
+    byte_plain_text = bytes(plain_text, "utf-8")
+    byte_key = bytes(key, "utf-8")
+    
     # Create cipher
-    # TODO: Set custom IV since we do not know what default is
-    cipher = AES.new(key, AES.MODE_CBC)
+    cipher = AES.new(byte_key, AES.MODE_CBC, b"0000000000000000")
 
     # Encrypt the text
-    cipher_text = cipher.encrypt(pad(plain_text, AES.block_size))
+    cipher_text = cipher.encrypt(pad(byte_plain_text, AES.block_size))
     
-    return cipher_text
+    return cipher_text, 200
+
+# Get request for decryption
+@app.route("/get-plain-text/")
+def get_decrypted_text():
+    cipher_text = request.args.get("ciphertext")
+    key = request.args.get("key")
+
+    return __undo_pirAtES__(cipher_text, key)
+
+# Function that undoes pirate substitution/decrypts
+def __undo_pirAtES__(cipher_text, key):
+    # Encode args so they play well with C
+    byte_cipher_text = bytes(cipher_text, "utf-8")
+    byte_key = bytes(key, "utf-8")
+
+    # Create cipher
+    cipher = AES.new(byte_key, AES.MODE_CBC, b"0000000000000000")
+
+    # Decrypt the text
+    plain_text = unpad(cipher.decrypt(bytearray(cipher_text)), AES.block_size)
+
+    return plain_text, 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
