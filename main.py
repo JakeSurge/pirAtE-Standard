ca34d49481145f8934c08af2b6fac80cf86bb2cf
Jake Surgeon <jakobsurge@gmail.com>
2024-11-13 09:39:00 -0500

    Added key length validation so it only supports key lengths of 16, 24, or 32 characters for different veresions of AES

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ def get_encrypted_text():
     # Validate data is not null
     if plain_text is None or key is None:
         return jsonify({'error': 'Missing JSON data'}), 400
+    # Validate key length
+    elif len(key) != 16 and len(key) != 24 and len(key) != 32:
+        return jsonify({'error': 'Improper key length only 16, 24, and 32 characters are supported'}), 400
 
     return jsonify(pirAtES(plain_text, key), 200)
 
@@ -38,6 +41,9 @@ def get_decrypted_text():
     # Validate data is not null
     if cipher_text is None or key is None:
         return jsonify({'error': 'Missing JSON data'}), 400
+    # Validate key length
+    elif len(key) != 16 and len(key) != 24 and len(key) != 32:
+        return jsonify({'error': 'Improper key length only 16, 24, and 32 characters are supported'}), 400
 
     return jsonify(unpirAtES(cipher_text, key), 200)
 
