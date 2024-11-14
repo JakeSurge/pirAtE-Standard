48214e54397e66345152d0a51a8f34be98f13788
Caburke1218 <caburke2@anderson.edu>
2024-11-14 00:34:15 -0500

    git commit -m "new ui/styling lots of clean up needed and still a WIP but good for now"

--- walktheplank/src/Anchor.tsx
+++ walktheplank/src/Anchor.tsx
@@ -3,6 +3,9 @@ import Button from "@mui/material/Button";
 import { TextField } from "@mui/material";
 import { useState } from "react";
 import { piratify, unpiratify } from "./Kraken";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import LockIcon from "@mui/icons-material/Lock";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
 
 export type ButtonType = "encrypt" | "decrypt";
 
@@ -10,39 +13,45 @@ export type BlackPearlProps = {
   image: string;
   input?: string;
   buttonText: string;
-  InputPlaceholder: string;
   buttonType: ButtonType;
+  InputPlaceholder: string;
+  KeyInputPlaceholder: string;
+  isSerious: boolean;
 };
 
 export const Anchor = ({
   image,
   buttonText,
-  InputPlaceholder,
   buttonType,
+  InputPlaceholder,
+  KeyInputPlaceholder,
+  isSerious,
 }: BlackPearlProps) => {
   const [outputVisible, setOutputVisible] = useState(false);
   const [outputText, setOutputText] = useState("");
   const [inputText, setInputText] = useState("");
+  const [keyInput, setKeyInput] = useState("");
 
   const translateToOrFromPirate = async (
     inputText: string,
+    keyInput: string,
     buttonType: ButtonType
   ) => {
     if (outputVisible) {
       setOutputText("");
     } else {
       if (buttonType === "encrypt") {
-        setOutputText(await piratify(inputText));
+        setOutputText(await piratify(inputText, keyInput));
       } else {
-        setOutputText(await unpiratify(inputText));
+        setOutputText(await unpiratify(inputText, keyInput));
       }
     }
     setOutputVisible(!outputVisible);
   };
 
-  const copyToClipboard = () => {
+  const copyToClipboard = (textToCopy: string) => {
     navigator.clipboard
-      .writeText(outputText)
+      .writeText(textToCopy)
       .then(() => {
         // You can add a notification here if you want to inform the user that the text has been copied
         console.log("Text copied to clipboard");
@@ -53,17 +62,18 @@ export const Anchor = ({
   };
 
   const isValidLength = (text: string): boolean => {
-    const validLengths = [16, 28, 32];
-    // Only check length validation for encrypt, always return true for decrypt
-    return buttonType === "decrypt" ? true : validLengths.includes(text.length);
+    const validLengths = [16, 24, 32];
+    return validLengths.includes(text.length);
   };
+
+  const buttonColor = isSerious ? "#f5f5f5" : "#4caf50";
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="w-full max-w-md flex flex-col items-center space-y-6">
         <img
           src={image}
-          alt="This was supposed to be a cool lego pirate"
+          alt="This was supposed to be a cool lego pirate or a serious pirate"
           className="w-64 h-auto object-cover"
         />
 
@@ -73,39 +83,56 @@ export const Anchor = ({
           variant="filled"
           className="w-full mb-4 "
           value={inputText}
+          multiline
           onChange={(e) => setInputText(e.target.value)}
-          error={
-            buttonType === "encrypt" &&
-            inputText.length > 0 &&
-            !isValidLength(inputText)
-          }
-          helperText={
-            buttonType === "encrypt" &&
-            inputText.length > 0 &&
-            !isValidLength(inputText)
-              ? "Text must be 16, 28, or 32 characters long"
-              : ""
-          }
           sx={{
-            backgroundColor: "#e0e0e0", // Light grey
+            backgroundColor: "#e0e0e0",
             "& .MuiFilledInput-root": {
-              color: "#37474f", // Darker color for text
+              color: "#37474f",
             },
             "& .MuiInputLabel-root": {
-              color: "#37474f", // Darker color for label
+              color: "#37474f",
             },
           }}
         />
-        <Button
-          onClick={() => translateToOrFromPirate(inputText, buttonType)}
-          className="mt-6 w-full px-4 py-2  text-white  transition duration-200"
-          variant="outlined"
-          disabled={buttonType === "encrypt" && !isValidLength(inputText)}
+
+        <TextField
+          id="filled-basic"
+          label={KeyInputPlaceholder}
+          variant="filled"
+          className="w-full mb-4 "
+          value={keyInput}
+          onChange={(e) => setKeyInput(e.target.value)}
+          error={keyInput.length > 0 && !isValidLength(keyInput)}
+          helperText={
+            keyInput.length > 0 && !isValidLength(keyInput)
+              ? "Text must be 16, 24, or 32 characters long"
+              : ""
+          }
           sx={{
-            backgroundColor: "#ff7043",
-            color: "#fff",
+            backgroundColor: "#e0e0e0",
+            "& .MuiFilledInput-root": {
+              color: "#37474f",
+            },
+            "& .MuiInputLabel-root": {
+              color: "#37474f",
+            },
+          }}
+        />
+
+        <Button
+          onClick={() =>
+            translateToOrFromPirate(inputText, keyInput, buttonType)
+          }
+          className="mt-6  px-4 py-2  text-white  transition duration-200"
+          variant="outlined"
+          disabled={buttonType === "encrypt" && !isValidLength(keyInput)}
+          startIcon={buttonType === "encrypt" ? <LockIcon /> : <LockOpenIcon />}
+          sx={{
+            backgroundColor: buttonColor,
+            color: isSerious ? "#000" : "#fff",
             "&:hover": {
-              backgroundColor: "#ff5722",
+              backgroundColor: isSerious ? "#e0e0e0" : "#45a049",
             },
           }}
         >
@@ -119,35 +146,54 @@ export const Anchor = ({
               value={outputText}
               variant="filled"
               className="w-full mb-4"
+              multiline
               slotProps={{
                 input: {
                   readOnly: true,
                 },
               }}
               sx={{
-                backgroundColor: "#e0e0e0", // Light grey
+                backgroundColor: "#e0e0e0",
                 "& .MuiFilledInput-root": {
-                  color: "#37474f", // Darker color for text
+                  color: "#37474f",
                 },
                 "& .MuiInputLabel-root": {
-                  color: "#37474f", // Darker color for label
+                  color: "#37474f",
                 },
               }}
             />
-            <Button
-              onClick={copyToClipboard}
-              className="mt-2 w-full px-4 py-2 text-white transition duration-200"
-              variant="outlined"
-              sx={{
-                backgroundColor: "#4caf50", // Green color
-                color: "#fff",
-                "&:hover": {
-                  backgroundColor: "#45a049", // Darker green on hover
-                },
-              }}
-            >
-              Copy to Clipboard
-            </Button>
+            <div className="flex space-x-3">
+              <Button
+                onClick={() => copyToClipboard(outputText)}
+                className="mt-2  px-4 py-2 text-white transition duration-200"
+                variant="outlined"
+                startIcon={<ContentCopyIcon />}
+                sx={{
+                  backgroundColor: buttonColor,
+                  color: isSerious ? "#000" : "#fff",
+                  "&:hover": {
+                    backgroundColor: isSerious ? "#e0e0e0" : "#45a049",
+                  },
+                }}
+              >
+                Copy Output
+              </Button>
+              <Button
+                onClick={() => copyToClipboard(keyInput)}
+                startIcon={<ContentCopyIcon />}
+                className="mt-2 px-4 py-2 text-white transition duration-200"
+                variant="outlined"
+                sx={{
+                  backgroundColor: buttonColor,
+                  color: isSerious ? "#000" : "#fff",
+                  "&:hover": {
+                    backgroundColor: isSerious ? "#e0e0e0" : "#45a049",
+                  },
+                }}
+              >
+                Copy Key
+              </Button>
+            </div>
           </>
         )}
       </div>
--- walktheplank/src/App.tsx
+++ walktheplank/src/App.tsx
@@ -1,16 +1,8 @@
 import { BlackPearl } from "./BlackPearl";
-import piratebg from "./piratebg.jpeg";
 
 function App() {
-  const appStyle = {
-    backgroundImage: `url(${piratebg})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    minHeight: "100vh",
-  };
-
   return (
-    <div style={appStyle}>
+    <div>
       <BlackPearl />
     </div>
   );
