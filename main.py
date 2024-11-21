cbaa42ab54e2ba302861dcbd6a701e9dbafd0f72
Jake Surgeon <jakobsurge@gmail.com>
2024-11-21 11:18:58 -0500

    Made the data validation go through a different function now since they used the same logic for making sure everything looks good. Will come in handy when adding IV support.

--- main.py
+++ main.py
@@ -12,33 +12,19 @@ CORS(app)
 def get_encrypted_text():
     # Grab JSON and args
     data = request.get_json()
-    
-    # Validate required arg data is there
-    try:
-        plaintext = data['plaintext']
-        key = data['key']['keyValue']
-        keyFormat = data['key']['keyFormat']
-    except:
-        return jsonify({'error': 'Missing JSON data'}), 400
-    
-    # Validate all data is string
-    if type(plaintext) != str and type(key) != str and type(keyFormat) != str:
-        return jsonify({'error', 'All required JSON values must be string'})
-    
-    # Get the key for both formats to byte object
-    if keyFormat.lower() == 'utf-8':
-        byte_key = key.encode()
-    elif keyFormat.lower() == 'base64':
-        try:
-            byte_key = base64.b64decode(key)
-        except:
-            return jsonify({'error': 'Improper Base64 key. It is is not formatted correctly.'}), 400
-    
-    # Validate key length
-    if len(byte_key) != 16 and len(byte_key) != 24 and len(byte_key) != 32:
-        return jsonify({'error': 'Improper key length'}), 400
 
-    return jsonify({'piratetext': pirAtES(plaintext.encode(), byte_key)}), 200
+    # Validate and encode data
+    args = __validate_and_format_data__(data, 'plaintext')
+
+    # If there was an error than return that
+    if type(args[1]) == int:
+        return args
+    
+    # Grab formatted data
+    plaintext = args[0]
+    key = args[1]
+
+    return jsonify({'piratetext': pirAtES(plaintext.encode(), key)}), 200
 
 # POST request for decryption
 @app.route('/unpiratify', methods=['POST'])
@@ -46,17 +32,41 @@ def get_decrypted_text():
     # Grab JSON and args
     data = request.get_json()
 
+    # Validate and encode data
+    args = __validate_and_format_data__(data, 'piratetext')
+
+    # If there was an error than return that
+    if type(args[1]) == int:
+        return args
+    
+    # Grab formatted data
+    piratetext = args[0]
+    key = args[1]
+
+    # Decrypt with try in case wrong password is used etc.
+    try:
+        plaintext = unpirAtES(piratetext, key)
+    except:
+        return (
+            jsonify(
+                {'error': 'An error ocurred while decrypting. Improper credentials or piratetext were most likely used.'}), 
+                500)
+    
+    return jsonify({'plaintext': plaintext}), 200
+
+# Function for validating and encoding JSON data sent for POST requests
+def __validate_and_format_data__(data, text_key):
     # Validate required arg data is there
     try:
-        piratetext = data['piratetext']
+        text = data[text_key]
         key = data['key']['keyValue']
         keyFormat = data['key']['keyFormat']
     except:
         return jsonify({'error': 'Missing JSON data'}), 400
     
-    # Validate all data is string
-    if type(piratetext) != str and type(key) != str and type(keyFormat) != str:
-        return jsonify({'error', 'All required JSON values must be string'})
+    # Validate all required data is string
+    if type(text) != str and type(key) != str and type(keyFormat) != str:
+        return jsonify({'error', 'All required JSON values must be string'}), 400
     
     # Get the key for both formats to byte object
     if keyFormat.lower() == 'utf-8':
@@ -66,21 +76,16 @@ def get_decrypted_text():
             byte_key = base64.b64decode(key)
         except:
             return jsonify({'error': 'Improper Base64 key. It is is not formatted correctly.'}), 400
+    else:
+        return jsonify({'error': 'Improper keyFormat specified. Use Base64 or UTF-8'})
     
     # Validate key length
     if len(byte_key) != 16 and len(byte_key) != 24 and len(byte_key) != 32:
         return jsonify({'error': 'Improper key length'}), 400
-
-    # Decrypt with try in case wrong password is used etc.
-    try:
-        plaintext = unpirAtES(piratetext, byte_key)
-    except:
-        return (
-            jsonify(
-                {'error': 'An error ocurred while decrypting. This could indicate improper credentials were used.'}), 
-                500)
     
-    return jsonify({'plaintext': plaintext}), 200
+    # Return validated data
+    return text, byte_key
+
 
 if __name__ == "__main__":
     app.run(debug=True)
